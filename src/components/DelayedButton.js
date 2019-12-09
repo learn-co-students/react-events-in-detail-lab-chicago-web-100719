ba6d24f6bf33bcaa47e741ec7c81b87c22ad3dca
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-12-09 17:50:10 -0600

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,12 @@
-// Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+class DelayedButton extends Component {
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default DelayedButton;
